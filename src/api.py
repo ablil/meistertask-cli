c21910d7da57eca5cadb220b2d5424154fc41f5f
ablil <ablil@protonmail.com>
2020-11-09 10:47:38 -0500

    ref: catch http calls errors

--- src/api.py
+++ src/api.py
@@ -7,27 +7,46 @@ from typing import List, Dict, Set
 from .utils import RED, END, match_names
 
 
+def catch_http_errors(func):
+    def wrapper(*args):
+        try:
+            return func(*args)
+        except requests.exceptions.ConnectionError as e:
+            print(f'{RED}It seems there is not internet connection{END}')
+            exit()
+        except Exception as e:
+            print(e)
+            exit()
+    return wrapper
+
 class API:
     def __init__(self, auth_key: str):
         self.auth_key = auth_key
         self.headers: Dict = {"Authorization": f"Bearer {self.auth_key}"}
 
+    @catch_http_errors
     def get_projects(self, filter_keyword="active") -> List[Dict]:
         r = requests.get(
             "https://www.meistertask.com/api/projects",
             headers=self.headers,
             params={"status": str(filter_keyword).strip().lower()},
         )
-
+        
+        if r.status_code != 200:
+            raise Exception("Failed to connect to API")
         return r.json()
 
+    @catch_http_errors
     def get_project(self, id: int) -> Dict:
         r = requests.get(
             f"https://www.meistertask.com/api/projects/{id}", headers=self.headers
         )
 
+        if r.status_code != 200:
+            raise Exception("Failed to connect to API")
         return r.json()
 
+    @catch_http_errors
     def create_project(self, name: str, description: str):
         r = requests.post(
             "https://www.meistertask.com/api/projects",
@@ -35,8 +54,11 @@ class API:
             data={"name": name, "notes": description},
         )
 
+        if r.status_code != 200:
+            raise Exception("Failed to connect to API")
         return r.json()
 
+    @catch_http_errors
     def update_project(self, id: int, name: str = "", description: str = ""):
 
         data = dict()
@@ -51,8 +73,11 @@ class API:
             data=data,
         )
 
+        if r.status_code != 200:
+            raise Exception("Failed to connect to API")
         return r.json()
 
+    @catch_http_errors
     def delete_project(self, id: int) -> Dict:
         r = requests.put(
             f"https://www.meistertask.com/api/projects/{id}",
@@ -60,8 +85,11 @@ class API:
             data={"status": 4},
         )
 
+        if r.status_code != 200:
+            raise Exception("Failed to connect to API")
         return r.json()
 
+    @catch_http_errors
     def archive_project(self, id: int) -> Dict:
         r = requests.put(
             f"https://www.meistertask.com/api/projects/{id}",
@@ -69,8 +97,11 @@ class API:
             data={"status": 5},
         )
 
+        if r.status_code != 200:
+            raise Exception("Failed to connect to API")
         return r.json()
 
+    @catch_http_errors
     def create_section(self, project_id: int, name: str) -> Dict:
         r = requests.post(
             f"https://www.meistertask.com/api/projects/{project_id}/sections",
@@ -78,16 +109,22 @@ class API:
             data={"name": str(name)},
         )
 
+        if r.status_code != 200:
+            raise Exception("Failed to connect to API")
         return r.json()
 
+    @catch_http_errors
     def get_tasks(self, project_id: int) -> List[Dict]:
         r = requests.get(
             f"https://www.meistertask.com/api/projects/{project_id}/tasks",
             headers=self.headers,
         )
 
+        if r.status_code != 200:
+            raise Exception("Failed to connect to API")
         return r.json()
 
+    @catch_http_errors
     def add_task(self, section_id: int, name: str, description: str = ""):
         r = requests.post(
             f"https://www.meistertask.com/api/sections/{section_id}/tasks",
@@ -95,8 +132,11 @@ class API:
             data={"name": str(name), "notes": str(description)},
         )
 
+        if r.status_code != 200:
+            raise Exception("Failed to connect to API")
         return r.json()
 
+    @catch_http_errors
     def update_task(self, id: int, name: str, description: str = ""):
         r = requests.put(
             f"https://www.meistertask.com/api/tasks/{id}",
@@ -104,8 +144,11 @@ class API:
             data={"name": str(name), "notes": str(description)},
         )
 
+        if r.status_code != 200:
+            raise Exception("Failed to connect to API")
         return r.json()
 
+    @catch_http_errors
     def move_task(self, task_id: int, section_id: int) -> Dict:
 
         r = requests.put(
@@ -114,8 +157,11 @@ class API:
             data={"section_id": section_id},
         )
 
+        if r.status_code != 200:
+            raise Exception("Failed to connect to API")
         return r.json()
 
+    @catch_http_errors
     def _get_project_by_name(self, name: str) -> List:
 
         projects = self.get_projects()
@@ -126,6 +172,7 @@ class API:
 
         return filtered_project
 
+    @catch_http_errors
     def _get_section_by_project(self, project_id: int) -> List[Dict]:
 
         r = requests.get(
@@ -133,8 +180,11 @@ class API:
             headers=self.headers,
         )
 
+        if r.status_code != 200:
+            raise Exception("Failed to connect to API")
         return r.json()
 
+    @catch_http_errors
     def _get_task_by_name(self, project_id: int, name: str) -> List[Dict]:
 
         r = requests.get(
